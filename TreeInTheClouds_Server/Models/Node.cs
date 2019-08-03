3be48688518ef3a3eb624d3731a0ad05134708bd
Demid Zhukov <1demidz3@gmail.com>
2019-08-04 02:39:07 +0300

    Models added; Nodes controller created
    LiteDB added

---
+++ TreeInTheClouds_Server/Models/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TreeInTheClouds_Server.Models
+{
+    public class Node
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public int Sequence { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        
+
+        public DateTime DateTime_Created { get; set; }
+        public DateTime DateTime_LastSaved { get; set; }
+
+        public string[] Tags { get; set; }
+
+        public List<Image> Images { get; set; }
+    }
+}
