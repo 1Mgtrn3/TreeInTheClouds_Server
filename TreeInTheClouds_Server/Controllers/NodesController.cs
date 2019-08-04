ecb54bc6653bb22487f2a069d7666fec71508632
sinxenon4 <sinxenon4@gmail.com>
2019-08-05 01:29:40 +0300

    Added some actual functionality to Nodes controller

--- TreeInTheClouds_Server/Controllers/NodesController.cs
+++ TreeInTheClouds_Server/Controllers/NodesController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LiteDB;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TreeInTheClouds_Server.Models;
 
 namespace TreeInTheClouds_Server.Controllers
 {
@@ -11,36 +14,203 @@ namespace TreeInTheClouds_Server.Controllers
     [ApiController]
     public class NodesController : ControllerBase
     {
-        // GET: api/Nodes
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly IConfiguration _configuration;
+        public string DbDir { get; set; }
+
+        private string GetDbPath(string fileName)
         {
-            return new string[] { "value1", "value2" };
+
+            return DbDir + fileName;
         }
 
-        // GET: api/Nodes/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public NodesController(IConfiguration configuration)
         {
-            return "value";
+            _configuration = configuration;
+            DbDir = _configuration.GetValue<string>("DbDirectory");
+        }
+        // GET: api/Nodes/findAll?fileName=testdb
+        [HttpGet("findAll")]
+        public IEnumerable<Node> Get(string fileName)
+        {
+            using (var Context = new LiteDatabase(GetDbPath(@fileName)))
+            {
+                var Nodes = Context.GetCollection<Node>(DbHelper.NodesCollection);
+                return Nodes.FindAll();
+            }
+
+            //new string[] { "value1", "value2" };
         }
 
-        // POST: api/Nodes
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [HttpGet("findAllAsync")]
+        public async Task<ActionResult<IEnumerable<Node>>> GetAsync(string fileName)
         {
+
+            return Ok(await GetAllNodesAsync(fileName).ConfigureAwait(false));
+
+
         }
 
-        // PUT: api/Nodes/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        private Task<IEnumerable<Node>> GetAllNodesAsync(string fileName)
         {
+            using (var Context = new LiteDatabase(GetDbPath(@fileName)))
+            {
+                return Task.Run(() => {
+
+                    var Nodes = Context.GetCollection<Node>(DbHelper.NodesCollection);
+                    return Nodes.FindAll();
+                });
+            };
+
+
         }
 
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // GET: api/Nodes/find?fileName=testdb&name=TestMan
+        [HttpGet("find")]
+        public Node Get(string fileName, string name)
         {
+            using (var Context = new LiteDatabase(GetDbPath(@fileName)))
+            {
+
+                return Context.GetCollection<Node>(DbHelper.NodesCollection).Find(x => x.Name == name).FirstOrDefault();
+            }
+
+
+        }
+
+
+        [HttpGet("findAsync")]
+        public async Task<ActionResult<Node>> GetAsync(string fileName, string name)
+        {
+
+            return Ok(await GetAsync(fileName, name).ConfigureAwait(false));
+
+        }
+
+        private Task<Node> GetNodeAsync(string fileName, string name)
+        {
+            using (var Context = new LiteDatabase(GetDbPath(@fileName)))
+            {
+                return Task.Run(() => {
+
+                    var Nodes = Context.GetCollection<Node>(DbHelper.NodesCollection);
+                    return Context.GetCollection<Node>(DbHelper.NodesCollection).Find(x => x.Name == name).FirstOrDefault();
+                });
+            };
+
+
+        }
+
+
+        // POST: api/Nodes/create?fileName=testdb
+        [HttpPost("create")]
+        public void Post(string fileName, Node Node)
+        {
+
+            using (var Context = new LiteDatabase(GetDbPath(@fileName)))
+            {
+                var Nodes = Context.GetCollection<Node>(DbHelper.NodesCollection);
+                Nodes.Insert(Node);
+                Nodes.EnsureIndex(x => x.Name);
+
+
+            }
+        }
+
+
+        [HttpPost("createAsync")]
+        public async void PostAsync(string fileName, Node Node)
+        {
+
+            await CreateNodeAsync(fileName, Node).ConfigureAwait(false);
+
+        }
+
+
+        private Task CreateNodeAsync(string fileName, Node Node)
+        {
+            using (var Context = new LiteDatabase(GetDbPath(@fileName)))
+            {
+                return Task.Run(() => {
+
+                    var Nodes = Context.GetCollection<Node>(DbHelper.NodesCollection);
+                    Nodes.Insert(Node);
+                    Nodes.EnsureIndex(x => x.Name);
+                });
+            };
+
+        }
+
+
+        // PUT: api/Nodes/edit?fileName=testdb
+        [HttpPut("edit")]
+        public void Put(string fileName, Node Node)
+        {
+            using (var Context = new LiteDatabase(GetDbPath(@fileName)))
+            {
+
+                var Nodes = Context.GetCollection<Node>(DbHelper.NodesCollection);
+                Nodes.Update(Node);
+            }
+
+        }
+
+
+
+        [HttpPut("editAsync")]
+        public async void PutAsync(string fileName, Node Node)
+        {
+
+            await EditNodeAsync(fileName, Node).ConfigureAwait(false);
+
+        }
+
+        private Task EditNodeAsync(string fileName, Node Node)
+        {
+            using (var Context = new LiteDatabase(GetDbPath(@fileName)))
+            {
+                return Task.Run(() => {
+
+                    var Nodes = Context.GetCollection<Node>(DbHelper.NodesCollection);
+                    Nodes.Update(Node);
+                });
+            };
+
+        }
+
+
+
+        // DELETE: api/ApiWithActions/delete?fileName=testdb&id=5
+        [HttpDelete("delete")]
+        public void Delete(string fileName, int id)
+        {
+
+            using (var Context = new LiteDatabase(GetDbPath(@fileName)))
+            {
+                var Nodes = Context.GetCollection<Node>(DbHelper.NodesCollection);
+                Nodes.Delete(id);
+
+            }
+        }
+
+        [HttpDelete("deleteAsync")]
+        public async void DeleteAsync(string fileName, int id)
+        {
+
+            await DeleteNodeAsync(fileName, id).ConfigureAwait(false);
+
+        }
+        private Task DeleteNodeAsync(string fileName, int id)
+        {
+            using (var Context = new LiteDatabase(GetDbPath(@fileName)))
+            {
+                return Task.Run(() => {
+
+                    var Nodes = Context.GetCollection<Node>(DbHelper.NodesCollection);
+                    Nodes.Delete(id);
+                });
+            };
+
         }
     }
 }
+
---
+++ TreeInTheClouds_Server/DbHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TreeInTheClouds_Server
+{
+    public static class DbHelper
+    {
+        public static string NodesCollection = "Nodes";
+    }
+}
